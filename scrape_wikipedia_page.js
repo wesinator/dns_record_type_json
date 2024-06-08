839ebea8ed8ef52afbd643e7eacc9d65a5ed6b4a
Wes <5124946+wesinator@users.noreply.github.com>
2024-06-07 21:54:00 -0400

    Update scrape_wikipedia_page.js
     - add download of json data
     - parse RFC text better, handle stray &nbsp; characters

--- scrape_wikipedia_page.js
+++ scrape_wikipedia_page.js
@@ -4,12 +4,18 @@ var records = [];
 $('table:first tbody tr').each(function(i, tr) {
    var kids = $(tr).children().toArray();
   records.push({
-    type: $.trim($(kids[0]).text()),
-    decimal_value: parseInt($(kids[1]).text()),
-    rfc: $(kids[2]).text(),
-    description: $(kids[3]).text(),
-    func: $(kids[4]).text(),
+    type: $.trim($(kids[0])[0].innerText),
+    decimal_value: parseInt($(kids[1])[0].innerText),
+    rfc: $(kids[2])[0].innerText.replace(0xa0, ' '),
+    description: $(kids[3])[0].innerText.replace(0xa0, ' '),
+    func: $(kids[4])[0].innerText.replace(0xa0, ' '),
   
   });
 });
-JSON.stringify(records);
+var out = JSON.stringify(records, null, 4);
+console.log(out);
+
+var a = document.createElement("a");
+a.href = `data:application/json;charset=utf-8,` + encodeURIComponent(out);
+a.download = "dns_record_types_wiki.json";
+a.click();
